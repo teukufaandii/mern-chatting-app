37134c2491abcc35ad39be2242fec6e9784abaa3
Teuku Fandi Ahmad <tfandiahmad32@gmail.com>
2025-04-26 22:19:21 +0700

    signup controller and adding jwt upon signing up

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,26 +1,47 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const signupHandler = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
-    const existingUser = await User.findOne({ email });
+    
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
 
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
     }
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = new User({
+    const newUser = new User({
       fullName,
       email,
       password: hashedPassword,
     });
 
-    await user.save();
-
-    res.status(201).json({ message: "User created successfully" });
+    if (newUser) {
+      // generate a JWT token
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePicture: newUser.profilePicture,
+      });
+    } else {
+      res.status(400).json({ message: "User registration failed" });
+    }
   } catch (error) {
     console.error("Error during signup:", error);
     res.status(500).json({ message: "Internal server error" });
